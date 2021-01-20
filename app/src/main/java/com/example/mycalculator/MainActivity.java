6ac62799834e3a2d2244143bf76c044c97e531e2
Shanthan-12 <shantan749@gmail.com>
2021-01-20 18:55:02 +0530

    Second Commit

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -36,5 +37,70 @@ boolean Add,Sub,Mul,Div;
         btnClear=(Button)findViewById(R.id.btnClear);
         ed1=(EditText) findViewById(R.id.editTextTextPersonName);
 
+        btn1.setOnClickListener(new View.OnClickListener()){
+            @Override
+        public void onClick(View v)     {
+                    ed1.setText(ed1.getText()+"1");
+        }
     }
+        btn2.setOnClickListener(new View.OnClickListener()){
+            @Override
+            public void onClick(View v)     {
+                ed1.setText(ed1.getText()+"2");
+            }
+        }
+        btn3.setOnClickListener(new View.OnClickListener()){
+            @Override
+            public void onClick(View v)     {
+                ed1.setText(ed1.getText()+"3");
+            }
+        }
+        btn4.setOnClickListener(new View.OnClickListener()){
+            @Override
+            public void onClick(View v)     {
+                ed1.setText(ed1.getText()+"4");
+            }
+        }
+        btn5.setOnClickListener(new View.OnClickListener()){
+            @Override
+            public void onClick(View v)     {
+                ed1.setText(ed1.getText()+"5");
+            }
+        }
+        btn6.setOnClickListener(new View.OnClickListener()){
+            @Override
+            public void onClick(View v)     {
+                ed1.setText(ed1.getText()+"6");
+            }
+        }
+        btn7.setOnClickListener(new View.OnClickListener()){
+            @Override
+            public void onClick(View v)     {
+                ed1.setText(ed1.getText()+"7");
+            }
+        }
+        btn8.setOnClickListener(new View.OnClickListener()){
+            @Override
+            public void onClick(View v)     {
+                ed1.setText(ed1.getText()+"8");
+            }
+        }
+        btn9.setOnClickListener(new View.OnClickListener()){
+            @Override
+            public void onClick(View v)     {
+                ed1.setText(ed1.getText()+"9");
+            }
+        }
+        btn0.setOnClickListener(new View.OnClickListener()){
+            @Override
+            public void onClick(View v)     {
+                ed1.setText(ed1.getText()+"0");
+            }
+        }
+        btnDot.setOnClickListener(new View.OnClickListener()){
+            @Override
+            public void onClick(View v)     {
+                ed1.setText(ed1.getText()+".");
+            }
+        }
 }
